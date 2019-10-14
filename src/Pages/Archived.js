e2325fd7fac16ada87db7a1caa8c4a6ed4d5b177
dfrempah <dougyboy14@gmail.com>
2019-10-14 12:08:31 +0000

    fixed trash and archived pages

--- src/Pages/Archived.js
+++ src/Pages/Archived.js
@@ -1,7 +1,16 @@
 import React from "react";
+import { GoFile } from "react-icons/go";
 
 export const Archived = () => (
-  <div>
-    <h1>Archived Page</h1>
+  <div className="pages-page">
+    <div className="pages-heading">
+      <h3>Archives</h3>
+    </div>
+    <div className="pages-content">
+      <GoFile size="70px" className="pages-center-icon" />
+      <p style={{ textAlign: "center" }}>
+        Archives Empty, add notes to archives to get started!
+      </p>
+    </div>
   </div>
 );
--- src/Pages/Trash.js
+++ src/Pages/Trash.js
@@ -1,3 +1,16 @@
 import React from "react";
+import { GoTrashcan } from "react-icons/go";
 
-export const Trash = () => <h1>Trash Page</h1>;
+export const Trash = () => (
+  <div className="pages-page">
+    <div className="pages-heading">
+      <h3>Trash</h3>
+    </div>
+    <div className="pages-content">
+      <GoTrashcan size="70px" className="pages-center-icon" />
+      <p style={{ textAlign: "center" }}>
+        Trash Empty, delete notes to get started!
+      </p>
+    </div>
+  </div>
+);
